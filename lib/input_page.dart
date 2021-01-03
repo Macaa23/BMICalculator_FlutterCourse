73bf73643053e6a856b39b7a5f58cb5ff938f77e
Rocío Sepúlveda <rocio_23s@hotmail.com>
2021-01-03 16:21:39 +0100

    [Maca] adds content to cards with customized widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -15,6 +15,8 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
   int personHeight = 160;
+  int weight = 70;
+  int age = 25;
 
   @override
   Widget build(BuildContext context) {
@@ -88,17 +90,25 @@ class _InputPageState extends State<InputPage> {
                       ),
                     ],
                   ),
-                  Slider(
-                    value: personHeight.toDouble(),
-                    min: 100.0,
-                    max: 220.0,
-                    activeColor: Color(0xFF7047c7),
-                    inactiveColor: Color(0x617047c7),
-                    onChanged: (double newValue) {
-                      setState(() {
-                        personHeight = newValue.round();
-                      });
-                    },
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: personHeight.toDouble(),
+                      min: 100.0,
+                      max: 220.0,
+                      activeColor: Color(0xFF7047c7),
+                      inactiveColor: Color(0x617047c7),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          personHeight = newValue.round();
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
@@ -110,11 +120,85 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: RoundedCard(
                     colors: gradientColorsInactive,
+                    cardContent: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'WEIGHT',
+                          style: textStyle,
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: numberTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundedIconButton(
+                              icon: FontAwesomeIcons.minus,
+                              state: () {
+                                setState(() {
+                                  weight--;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            RoundedIconButton(
+                              icon: FontAwesomeIcons.plus,
+                              state: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                     child: RoundedCard(
                   colors: gradientColorsInactive,
+                  cardContent: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'AGE',
+                        style: textStyle,
+                      ),
+                      Text(
+                        age.toString(),
+                        style: numberTextStyle,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          RoundedIconButton(
+                            icon: FontAwesomeIcons.minus,
+                            state: () {
+                              setState(() {
+                                age--;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          RoundedIconButton(
+                            icon: FontAwesomeIcons.plus,
+                            state: () {
+                              setState(() {
+                                age++;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 )),
               ],
             ),
@@ -127,6 +211,26 @@ class _InputPageState extends State<InputPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class RoundedIconButton extends StatelessWidget {
+  RoundedIconButton({@required this.icon, @required this.state});
+  final IconData icon;
+  final Function state;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: state,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF908fe7),
     );
   }
 }
